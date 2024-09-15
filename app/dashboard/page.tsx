ac4e52407d3b94ec8b423a6977141ae19f281f4e
CaptainSlow22 <razvandumitru2002@yahoo.com>
2024-09-15 12:52:13 +0300

    dashboard useEffect fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,18 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import { useSession } from 'next-auth/react';
-import DashboardCard from '@/components/DashboardCard';
-import { CircleDollarSign, PiggyBank, ReceiptText } from 'lucide-react';
-import { financialAdvice } from '@/lib/financialAdvice';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import BudgetCard from '@/components/BudgetCard';
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import DashboardCard from "@/components/DashboardCard";
+import { CircleDollarSign, PiggyBank, ReceiptText } from "lucide-react";
+import { financialAdvice } from "@/lib/financialAdvice";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import BudgetCard from "@/components/BudgetCard";
 
 interface User {
   id: string;
@@ -41,6 +48,11 @@ const Dashboard = () => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
 
+  const [loadingUser, setLoadingUser] = useState(true);
+  const [loadingBudgets, setLoadingBudgets] = useState(true);
+  const [loadingIncomes, setLoadingIncomes] = useState(true);
+  const [loadingExpenses, setLoadingExpenses] = useState(true);
+
   const fetchBudgets = async (userId: string) => {
     try {
       const res = await fetch(`/api/get/getBudgets?userId=${userId}`);
@@ -50,6 +62,8 @@ const Dashboard = () => {
       setBudgets(budg.reverse());
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoadingBudgets(false);
     }
   };
 
@@ -62,6 +76,8 @@ const Dashboard = () => {
       setIncomes(inc.reverse());
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoadingIncomes(false); 
     }
   };
 
@@ -74,6 +90,21 @@ const Dashboard = () => {
       setExpenses(exp.reverse());
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoadingExpenses(false); 
+    }
+  };
+
+  const fetchUser = async (userId: string) => {
+    try {
+      const userRes = await fetch(`/api/get/getUserById/${userId}`);
+      if (!userRes.ok) throw new Error("Network response error");
+      const userData: User = await userRes.json();
+      setUser(userData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoadingUser(false);
     }
   };
 
@@ -93,14 +124,15 @@ const Dashboard = () => {
     if (!userId) return;
 
     const fetchData = async () => {
+      setLoading(true);
       try {
-        setLoading(true);
-        const userRes = await fetch(`/api/get/getUserById/${userId}`);
-        if (!userRes.ok) throw new Error("Network response error");
-        const userData: User = await userRes.json();
-        setUser(userData);
+        await fetchUser(userId);
 
-        await Promise.all([fetchBudgets(userId), fetchIncomes(userId), fetchExpenses(userId)]);
+        await Promise.all([
+          fetchBudgets(userId),
+          fetchIncomes(userId),
+          fetchExpenses(userId),
+        ]);
       } catch (error) {
         console.log(error);
       } finally {
@@ -116,12 +148,16 @@ const Dashboard = () => {
   const totalSpent = getTotalSpent();
 
   const latestExpenses = expenses.slice(0, 5);
-  const latestBudgets = budgets.slice(0,3);
+  const latestBudgets = budgets.slice(0, 3);
 
   useEffect(() => {
     const fetchAdvice = async () => {
       if (totalBudget > 0 || totalSpent > 0 || totalIncome > 0) {
-        const advice = await financialAdvice({totalBudget, totalSpent, totalIncome});
+        const advice = await financialAdvice({
+          totalBudget,
+          totalSpent,
+          totalIncome,
+        });
         setAdvice(advice);
       }
     };
@@ -129,52 +165,69 @@ const Dashboard = () => {
     fetchAdvice();
   }, [totalBudget, totalSpent, totalIncome]);
 
-  if (loading) return <div>Loading...</div>;
+  if (
+    loadingUser || 
+    loadingBudgets || 
+    loadingIncomes || 
+    loadingExpenses || 
+    loading
+  ) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className='p-4'>
-      <h1 className='text-4xl font-bold'>👋 Hi, {user?.name}</h1>
-      <div className='flex flex-col lg:grid lg:grid-cols-3 mt-8 p-4 gap-10'>
+    <div className="p-4">
+      <h1 className="text-4xl font-bold">👋 Hi, {user?.name}</h1>
+      <div className="flex flex-col lg:grid lg:grid-cols-3 mt-8 p-4 gap-10">
         <DashboardCard title="Total Budget" amount={totalBudget} Icon={PiggyBank} />
         <DashboardCard title="Total Spent" amount={totalSpent} Icon={ReceiptText} />
         <DashboardCard title="Total Income Streams" amount={totalIncome} Icon={CircleDollarSign} />
       </div>
-      <div className='p-4 mt-8 bg-green-200 rounded-2xl'>
-        <h2 className='text-4xl p-2 font-bold text-wrap'>✨BudgetWise AI Financial Advice</h2>
-        <p className='font-bold p-6 text-md'>{advice}</p>
+      <div className="p-4 mt-8 bg-gradient-to-br from-green-200 to-green-400  rounded-2xl">
+        <h2 className="text-4xl p-2 font-bold text-wrap">✨ BudgetWise AI Financial Advice</h2>
+        <p className="font-bold p-6 text-md">{advice}</p>
       </div>
-      <div className='flex flex-col lg:flex-row'>
-        <div className='p-4 lg:w-1/2'>
-            <h2 className='font-bold text-4xl'>Latest Expenses</h2>
-              <div className="mt-4">
-                  <Table>
-                      <TableHeader>
-                          <TableRow>
-                              <TableHead className="w-[200px]">Name</TableHead>
-                              <TableHead>Amount</TableHead>
-                              <TableHead>Date</TableHead>
-                          </TableRow>
-                      </TableHeader>
-                      <TableBody>
-                          {latestExpenses.map((expense) => (
-                              <TableRow key={expense._id}>
-                                  <TableCell className="font-bold">{expense.name}</TableCell>
-                                  <TableCell className="font-bold">{expense.amount}$</TableCell>
-                                  <TableCell className="font-bold">{new Date(expense.createdAt).toLocaleDateString()}</TableCell>
-                              </TableRow>
-                          ))}
-                      </TableBody>
-                  </Table>
-                </div>
-          </div>
-          <div className='p-4 lg:w-1/2'>
-            <h2 className='font-bold text-4xl'>Latest Budgets</h2>
-            <div className='overflow-y-auto mt-4 p-4 h-[350px] space-y-2'>
-                {latestBudgets.map((budget) => (
-                  <BudgetCard key={budget._id} _id={budget._id} icon={budget.icon} name={budget.name} amount={budget.amount} expenseAdded />
+      <div className="flex flex-col lg:flex-row">
+        <div className="p-4 lg:w-1/2">
+          <h2 className="font-bold text-4xl">Latest Expenses</h2>
+          <div className="mt-4">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[200px]">Name</TableHead>
+                  <TableHead>Amount</TableHead>
+                  <TableHead>Date</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {latestExpenses.map((expense) => (
+                  <TableRow key={expense._id}>
+                    <TableCell className="font-bold">{expense.name}</TableCell>
+                    <TableCell className="font-bold">{expense.amount}$</TableCell>
+                    <TableCell className="font-bold">
+                      {new Date(expense.createdAt).toLocaleDateString()}
+                    </TableCell>
+                  </TableRow>
                 ))}
-            </div>
+              </TableBody>
+            </Table>
           </div>
+        </div>
+        <div className="p-4 lg:w-1/2">
+          <h2 className="font-bold text-4xl">Latest Budgets</h2>
+          <div className="overflow-y-auto mt-4 p-4 h-[350px] space-y-2">
+            {latestBudgets.map((budget) => (
+              <BudgetCard
+                key={budget._id}
+                _id={budget._id}
+                icon={budget.icon}
+                name={budget.name}
+                amount={budget.amount}
+                expenseAdded
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
